2fd487d2475764cfbbcb1797416888ef5a027928
Bin <Bin@dubindeMacBook-Pro-2.local>
2015-01-23 17:04:26 +0800

    车主查询订单借口

--- Driver/Home/Controller/IndexController.class.php
+++ Driver/Home/Controller/IndexController.class.php
@@ -8,23 +8,24 @@ class IndexController extends BaseController {
 	private $lat;
 	private $lng;
 
-	public function _initialize(){
-		$uid = I('get.uid');
-		$uuid = I('get.uuid');
-		$data = $this->getUsercache($uid);
-		if($data){
-			if ($data['uuid'] == $uuid) {
-				$this->uid = $uid;
-				return;
-			}
-			else{
-				$this->ajaxFail();
-			}
-		}
-		else{
-			$this->ajaxFail();
-		}
-	}
+//	public function _initialize(){
+//		$uid = I('get.uid');
+//		$uuid = I('get.uuid');
+//		$this->uid = $uid;
+//		$data = $this->getUsercache($uid);
+//		if($data){
+//			if ($data['uuid'] == $uuid) {
+//				$this->uid = $uid;
+//				return;
+//			}
+//			else{
+//				$this->ajaxFail();
+//			}
+//		}
+//		else{
+//			$this->ajaxFail();
+//		}
+//	}
 
     public function index(){
         $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover,{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
@@ -145,6 +146,40 @@ class IndexController extends BaseController {
 
 		}
 
+	}
+
+	/*
+	 * @desc 查询最后的若干数量的订单，或者查询最新的一条未支付订单
+	 * @last int 0-所有订单/1-最后订单
+	*/
+
+	public  function getOrder($last){
+		$this->uid =1;
+		$Order = M('ParkOrder');
+		if($last){
+			$con['uid'] = $this->uid;
+			$con['state'] = array(0,1,'OR');
+		}
+		else{
+			$con['uid'] = $this->uid;
+		}
+
+		$orderData = $Order->where($con)->select();
+
+		$result = array();
+		foreach($orderData as $key => $value){
+			$tmp['oid'] = $value['id'];
+			$tmp['startTime'] = $value['startime'];
+
+			$Park = M('ParkInfo');
+			$parkInfo = $Park->where('id = '.$value['pid'])->find();
+			$tmp['parkname'] = $parkInfo['name'];
+			$tmp['address'] = $parkInfo['address'];
+
+			array_push($result, $tmp);
+		}
+
+		$this->ajaxOk($result);
 	}
 
 	//获得IP地址
