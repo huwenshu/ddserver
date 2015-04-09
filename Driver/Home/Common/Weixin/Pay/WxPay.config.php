1d1257dcd939362a24debc5ea394747115bf0d0e
joe <hzhou9@gmail.com>
2015-04-09 14:46:06 +0800

    new wx payment

--- Driver/Home/Common/Weixin/Pay/WxPay.config.php
+++ Driver/Home/Common/Weixin/Pay/WxPay.config.php
@@ -3,10 +3,10 @@
 * 
 */
 
-define(APPID , "wxd3c766afb2b3b774");  //appid
+define(APPID , "wxd417c2e70f817f89"/*"wxd3c766afb2b3b774"*/);  //appid
 define(APPKEY ,"9ur6qRqjSPf6dRS2leYVW3Hul5X8DdW6HOwUwYNBngh9IOt5FgGXgn6floIw55ga1GE1VlsLyEIwORojscP0Q73gCE6bnG6hHughGVabE5cL94BaeecBYtI9d51ipW4i"); //paysign key
 define(SIGNTYPE, "sha1"); //method
-define(PARTNERKEY,"0a870e4e9d1b1376587bd414316aed86");//通加密串
-define(APPSERCERT, "72fcf4b7f001dc9e37abaadbcc692c21");
+define(PARTNERKEY,"b91c8f3aab404cd391695c7bfcc86ae8"/*"0a870e4e9d1b1376587bd414316aed86"*/);//通加密串
+define(APPSERCERT, "14f025315fecb3bd1bdfc1624338605c"/*"72fcf4b7f001dc9e37abaadbcc692c21"*/);
 
 ?>
